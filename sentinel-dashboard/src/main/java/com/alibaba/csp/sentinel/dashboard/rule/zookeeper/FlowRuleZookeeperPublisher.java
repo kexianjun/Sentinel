7f213288e6ae130f3623c972d6cee02e85166866
kexianjun <kexianjun1991@hotmail.com>
2019-04-28 11:03:48 +0800

    dashboard support zookeeper

---
+++ sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/rule/zookeeper/FlowRuleZookeeperPublisher.java
@@ -0,0 +1,35 @@
+package com.alibaba.csp.sentinel.dashboard.rule.zookeeper;
+
+import com.alibaba.csp.sentinel.dashboard.datasource.entity.rule.FlowRuleEntity;
+import com.alibaba.csp.sentinel.dashboard.rule.DynamicRulePublisher;
+import com.alibaba.csp.sentinel.datasource.Converter;
+import com.alibaba.csp.sentinel.util.AssertUtil;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.data.Stat;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component("flowRuleZookeeperPublisher")
+public class FlowRuleZookeeperPublisher implements DynamicRulePublisher<List<FlowRuleEntity>> {
+    @Autowired
+    private CuratorFramework zkClient;
+    @Autowired
+    private Converter<List<FlowRuleEntity>, String> converter;
+
+    @Override
+    public void publish(String app, List<FlowRuleEntity> rules) throws Exception {
+        AssertUtil.notEmpty(app, "app name cannot be empty");
+        if (null == rules) {
+            return;
+        }
+        String path = ZookeeperConfigUtil.getPath(app);
+        Stat stat = zkClient.checkExists().forPath(path);
+        if (stat == null) {
+            zkClient.create().creatingParentContainersIfNeeded().withMode(CreateMode.PERSISTENT).forPath(path, null);
+        }
+        zkClient.setData().forPath(path, converter.convert(rules).getBytes());
+    }
+}
---
+++ sentinel-dashboard/src/test/java/com/alibaba/csp/sentinel/dashboard/rule/zookeeper/FlowRuleZookeeperProvider.java
@@ -0,0 +1,31 @@
+package com.alibaba.csp.sentinel.dashboard.rule.zookeeper;
+
+import com.alibaba.csp.sentinel.dashboard.datasource.entity.rule.FlowRuleEntity;
+import com.alibaba.csp.sentinel.dashboard.rule.DynamicRuleProvider;
+import com.alibaba.csp.sentinel.datasource.Converter;
+import org.apache.curator.framework.CuratorFramework;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component("flowRuleZookeeperPublisher")
+public class FlowRuleZookeeperProvider implements DynamicRuleProvider<List<FlowRuleEntity>> {
+
+    @Autowired
+    private CuratorFramework zkClient;
+    @Autowired
+    private Converter<String, List<FlowRuleEntity>> converter;
+
+    @Override
+    public List<FlowRuleEntity> getRules(String appName) throws Exception {
+        String zkPath = ZookeeperConfigUtil.getPath(appName);
+        byte[] bytes = zkClient.getData().forPath(zkPath);
+        if (null == bytes || bytes.length == 0) {
+            return null;
+        }
+        String s = new String(bytes);
+
+        return converter.convert(s);
+    }
+}
